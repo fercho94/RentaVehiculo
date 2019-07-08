007e9d2fc04e031cd892fbdfd3cf8ce948e7d969
José Fernando Lanza Beltrán <fercho94@hotmail.com>
2019-07-08 11:54:29 -0500

    Modificacion de la clase carros
    Se pasa la informacion a un txt para importarla

--- src/rentaautomoviles/Carros.java
+++ src/rentaautomoviles/Carros.java
@@ -3,10 +3,10 @@ package rentaautomoviles;
 
 public class Carros 
 {
-   public String [] placas = {"FGT456","AZB321","MUF407","XLS777","DOG682"};
-   public String [] modelos = {"R4","Ferrari","Mazda323","lamborghini","BMW"};
-   public double [] precios = {100000,20000,50000,600000,1200000};
-   public boolean[] disponible = {true,true,true,true,true};
+   public String [] placas = new String [5];
+   public String [] modelos = new String [5];
+   public double [] precios = new double [5];
+   public boolean[] disponible = new boolean [5];
 }
 
 
